dde11978f5f62260a575d8f3d1627433c0017d29
zkman420 <kith4919@gmail.com>
2024-06-05 18:43:23 +1000

    Main + Multi Working + web Server Check

--- class-api/index.js
+++ class-api/index.js
@@ -7,7 +7,7 @@ const mysql = require("mysql2");
 const app = express();
 
 const pushoverToken = "agc7po6u59qxzcf56qozp7bmjudhgi";
-const userKey = "u6shfndeu1vn8tu1yc3w6vdtn93rxj";
+let userKey = "u6shfndeu1vn8tu1yc3w6vdtn93rxj";
 
 const articles = [];
 
@@ -27,27 +27,32 @@ app.use((req, res, next) => {
   if (apiKey !== process.env.API_KEY) {
     return res.status(401).json({ error: "Unauthorized" });
   }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   next();
 });
 
 async function fetchClassInfo(period) {
+
   console.log("fetch " + period);
   const cookies = [
     {
       name: "SESSIONID",
-      value: "FC458205%2DFEDD%2D30D7%2D90A276511A417B5F",
+      value: "C27055CF%2DB252%2D5277%2DE714DD72901A3DFE",
     },
     {
       name: "SESSIONTOKEN",
-      value: "FC458204%2DAD51%2DDDF8%2D8E9C493201B6C437",
+      value: "C27055CE%2DFE2F%2DCB13%2D8A07FDA8AE5B85ED",
     },
     {
       name: "CFID",
-      value: "5080134",
+      value: "5065946",
     },
     {
       name: "CFTOKEN",
-      value: "4740c4c68913e9dc-FC4495A9-0326-4FD6-EF2AA063DCBC0CDB",
+      value: "95cd02481515a22e-C26FC4D9-E905-B53B-3A05A62C31CA15A4",
     },
   ];
 
@@ -106,6 +111,65 @@ async function fetchClassInfo(period) {
   }
 }
 
+async function fetchClassInfoUser(user, period, cookies, userKey) {
+    console.log(`Fetching class info for ${user.username} - ${period}`);
+
+    const cookieString = `SESSIONID=${cookies.SESSIONID}; SESSIONTOKEN=${cookies.SESSIONTOKEN}; CFID=${cookies.CFID}; CFTOKEN=${cookies.CFTOKEN}`;
+
+  try {
+    const response = await axios.get(
+      "https://tassweb.salc.qld.edu.au/studentcafe/index.cfm?do=studentportal.home#timetable",
+      {
+        headers: {
+          Cookie: cookieString,
+        },
+      }
+    );
+
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const classes = [];
+
+    $("tr").each((index, element) => {
+      const periodElement = $(element).find("td").eq(0).text().trim();
+      const classInfoElement = $(element).find("td").eq(1).text().trim();
+      const locationElement = $(element).find("td").eq(2).text().trim();
+      const teacherElement = $(element).find("td").eq(3).text().trim();
+      if (periodElement.includes(`${period}`)) {
+        const classInfo = {
+          period: periodElement,
+          class: classInfoElement,
+          location: locationElement,
+          teacher: teacherElement,
+        };
+        console.log(classInfo); // Output the class information to the console
+        classes.push(classInfo);
+      }
+    });
+
+    // Send notification if classes are found
+    if (classes.length > 0) {
+      const message = {
+        token: pushoverToken,
+        user: userKey,
+        message: `Hello! ${user.username} Your next class is ${classes[0].class} at ${classes[0].location} with ${classes[0].teacher}`,
+      };
+
+      await axios.post("https://api.pushover.net/1/messages.json", message);
+    } else {
+      console.log("No classes found.");
+    }
+
+    return classes;
+  } catch (error) {
+    console.error("Error fetching class info:", error);
+    throw error;
+  }
+
+}
+app.get("/", (req, res) => {
+  res.send("im alive");
+})
 // Endpoint to post
 app.post("/send/:period", async (req, res) => {
   try {
@@ -118,85 +182,130 @@ app.post("/send/:period", async (req, res) => {
   }
 });
 
-app.post('/addUser', async (req, res) => {
-  const { username, userKey } = req.body;
-
-  if (!username || !userKey) {
-    return res.status(400).json({ error: "Username, userKey are required." });
-  }
-
+app.post("/sendUsers/:period", async (req, res) => {
   try {
-    const sql = "INSERT INTO Users (username, userKey) VALUES (?, ?)";
-    const values = [username, userKey];
-
-    pool.query(sql, values, (err, result) => {
+    let period = req.params;
+    
+    const sql = "SELECT * FROM Users";
+    pool.query(sql, (err, result) => {
       if (err) {
-        console.error("Error inserting user: " + err);
-        return res.status(500).json({ error: "An error occurred while adding the user." });
+        console.error("Error fetching users: " + err);
+        return res.status(500).json({ error: "An error occurred while fetching the users." });
       }
-      console.log("User added successfully.");
-      res.status(200).json({ message: "User added successfully." });
+      console.log("Users fetched successfully.");
+      
+      let classes = [];
+      result.forEach(async (user) => {
+        const cookies = await getCookiesForUser(user);
+        console.log("parsed data")
+        classes.push(...await fetchClassInfoUser(user, period.period, cookies, user.userKey));
+      });
+      res.json(classes);
     });
-  } catch (err) {
-    console.error("Error hashing password:", err);
-    res.status(500).json({ error: "An error occurred while hashing the password" });
+  } catch (error) { 
+    if (res.headersSent) {
+      console.error(error);
+    } else {
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
 });
 
-app.post('/addcookies', async (req, res) => {
-  const { UserID, CFID, CFTOKEN, SESSIONID, SESSIONTOKEN } = req.body;
-
-  if (!UserID || !CFID || !CFTOKEN || !SESSIONID || !SESSIONTOKEN) {
-    return res.status(400).json({ error: "UserID, CFID, CFTOKEN, SESSIONID, SESSIONTOKEN are required." });
-  }
-
-  try {
-    const sql = "INSERT INTO `Cookies` (`UserID`, `CFID`, `CFTOKEN`, `SESSIONID`, `SESSIONTOKEN`) VALUES (?, ?, ?, ?, ?)";
-    const values = [UserID, CFID, CFTOKEN, SESSIONID, SESSIONTOKEN];
-
-    pool.query(sql, values, (err, result) => {
+async function getCookiesForUser(user) {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT CFID, CFTOKEN, SESSIONID, SESSIONTOKEN FROM Cookies WHERE UserID = ?";
+    pool.query(sql, [user.UserID], (err, result) => {
       if (err) {
-        console.error("Error inserting user: " + err);
-        return res.status(500).json({ error: "An error occurred while adding the cookies." });
+        console.error("Error fetching cookies for user: " + err);
+        reject(err);
       }
-      console.log("User added successfully.");
-      res.status(200).json({ message: "cookies added successfully." });
+      console.log("Cookies fetched successfully for user.");
+      resolve(result[0]);
     });
-  } catch (err) {
-    console.error("Error hashing password:", err);
-    res.status(500).json({ error: "An error occurred " });
-  }
-});
+  });
+}
+
+// app.post('/addUser', async (req, res) => {
+
+//   const { username, userKey } = req.body;
+
+//   if (!username || !userKey) {
+//     return res.status(400).json({ error: "Username, userKey are required." });
+//   }
+
+//   try {
+//     const sql = "INSERT INTO Users (username, userKey) VALUES (?, ?)";
+//     const values = [username, userKey];
+
+//     pool.query(sql, values, (err, result) => {
+//       if (err) {
+//         console.error("Error inserting user: " + err);
+//         return res.status(500).json({ error: "An error occurred while adding the user." });
+//       }
+//       console.log("User added successfully.");
+//       res.status(200).json({ message: "User added successfully." });
+//     });
+//   } catch (err) {
+//     console.error("Error hashing password:", err);
+//     res.status(500).json({ error: "An error occurred while hashing the password" });
+//   }
+// });
+
+// app.post('/addcookies', async (req, res) => {
+//   const { UserID, CFID, CFTOKEN, SESSIONID, SESSIONTOKEN } = req.body;
+
+//   if (!UserID || !CFID || !CFTOKEN || !SESSIONID || !SESSIONTOKEN) {
+//     return res.status(400).json({ error: "UserID, CFID, CFTOKEN, SESSIONID, SESSIONTOKEN are required." });
+//   }
+
+//   try {
+//     const sql = "INSERT INTO `Cookies` (`UserID`, `CFID`, `CFTOKEN`, `SESSIONID`, `SESSIONTOKEN`) VALUES (?, ?, ?, ?, ?)";
+//     const values = [UserID, CFID, CFTOKEN, SESSIONID, SESSIONTOKEN];
+
+//     pool.query(sql, values, (err, result) => {
+//       if (err) {
+//         console.error("Error inserting user: " + err);
+//         return res.status(500).json({ error: "An error occurred while adding the cookies." });
+//       }
+//       console.log("User added successfully.");
+//       res.status(200).json({ message: "cookies added successfully." });
+//     });
+//   } catch (err) {
+//     console.error("Error hashing password:", err);
+//     res.status(500).json({ error: "An error occurred " });
+//   }
+// });
 
 // get 
 
-app.get('/getCookies', async (req, res) => {
-  const sql = "SELECT * FROM Cookies";
-  pool.query(sql, (err, result) => {
-    if (err) {
-      console.error("Error fetching cookies: " + err);
-      return res.status(500).json({ error: "An error occurred while fetching the cookies." });
-    }
-    console.log("Cookies fetched successfully.");
-    res.status(200).json(result);
-  });
-});
-app.get('/getUsers', async (req, res) => {
-  const sql = "SELECT * FROM Users";
-  pool.query(sql, (err, result) => {
-    if (err) {
-      console.error("Error fetching users: " + err);
-      return res.status(500).json({ error: "An error occurred while fetching the users." });
-    }
-    console.log("Users fetched successfully.");
-    res.status(200).json(result);
-  });
-})
+// app.get('/getCookies', async (req, res) => {
+//   const sql = "SELECT * FROM Cookies";
+//   pool.query(sql, (err, result) => {
+//     if (err) {
+//       console.error("Error fetching cookies: " + err);
+//       return res.status(500).json({ error: "An error occurred while fetching the cookies." });
+//     }
+//     console.log("Cookies fetched successfully.");
+//     res.status(200).json(result);
+//   });
+// });
+// app.get('/getUsers', async (req, res) => {
+//   const sql = "SELECT * FROM Users";
+//   pool.query(sql, (err, result) => {
+//     if (err) {
+//       console.error("Error fetching users: " + err);
+//       return res.status(500).json({ error: "An error occurred while fetching the users." });
+//     }
+//     console.log("Users fetched successfully.");
+//     res.status(200).json(result);
+//   });
+// })
 
 
 
 // Schedule the task to run at a specific time
-cron.schedule("55 8 * * *", async () => {
+
+cron.schedule("54 8 * * *", async () => {
   console.log("Schedule");
   try {
     let period = "Period 1";
@@ -209,7 +318,7 @@ cron.schedule("55 8 * * *", async () => {
   }
 });
 
-cron.schedule("45 9 * * *", async () => {
+cron.schedule("44 9 * * *", async () => {
   console.log("Schedule");
   try {
     let period = "Period 2";
@@ -222,7 +331,7 @@ cron.schedule("45 9 * * *", async () => {
   }
 });
 
-cron.schedule("00 11 * * *", async () => {
+cron.schedule("54 10 * * *", async () => {
   console.log("Schedule");
   try {
     let period = "Period 3";
@@ -234,7 +343,7 @@ cron.schedule("00 11 * * *", async () => {
     console.error("Scheduled task failed:", error);
   }
 });
-cron.schedule("50 11 * * *", async () => {
+cron.schedule("45 11 * * *", async () => {
   console.log("Schedule");
   try {
     let period = "Period 4";
@@ -247,7 +356,7 @@ cron.schedule("50 11 * * *", async () => {
   }
 });
 
-cron.schedule("25 13 * * *", async () => {
+cron.schedule("24 13 * * *", async () => {
   console.log("Schedule");
   try {
     let period = "Period 5";
@@ -260,7 +369,7 @@ cron.schedule("25 13 * * *", async () => {
   }
 });
 
-cron.schedule("20 14 * * *", async () => {
+cron.schedule("14 14 * * *", async () => {
   console.log("Schedule");
   try {
     let period = "Period 5";
@@ -277,3 +386,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
--- classes-api.session.sql
+++ classes-api.session.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `Users` (
     `UserID` INT AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(255) NOT NULL UNIQUE,
-    `userKey` VARCHAR(255) NOT NULL UNIQUE
+    `userKey` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE `Cookies` (
@@ -21,12 +21,16 @@ SELECT * FROM Users
 --@block
 SHOW TABLES;
 --@block
+DROP TABLE Cookies, Users;
+--@block // kian
 INSERT INTO `Users` (`username`, `userKey`) VALUES
-('user1', 'key1'),
-('user2', 'key2'),
-('user3', 'key3');
-INSERT INTO `Cookies` (`UserID`, `CFID`, `CFTOKEN`, `SESSIONID`, `SESSIONTOKEN`) VALUES
-(1, 'cfid1', 'cftoken1', 'sessionid1', 'sessiontoken1'),
-(2, 'cfid2', 'cftoken2', 'sessionid2', 'sessiontoken2'),
-(3, 'cfid3', 'cftoken3', 'sessionid3', 'sessiontoken3');
+('Kian', 'u6shfndeu1vn8tu1yc3w6vdtn93rxj');
 
+INSERT INTO `Cookies` (`UserID`, `CFID`, `CFTOKEN`, `SESSIONID`, `SESSIONTOKEN`) VALUES
+(1, '5065946', '95cd02481515a22e-C26FC4D9-E905-B53B-3A05A62C31CA15A4', 'C27055CF%2DB252%2D5277%2DE714DD72901A3DFE', 'C27055CE%2DFE2F%2DCB13%2D8A07FDA8AE5B85ED');
+--@block // jack
+INSERT INTO `Users` (`username`, `userKey`) VALUES
+('Jack', 'u6shfndeu1vn8tu1yc3w6vdtn93rxj');
+
+INSERT INTO `Cookies` (`UserID`, `CFID`, `CFTOKEN`, `SESSIONID`, `SESSIONTOKEN`) VALUES
+(2, '5080134', '4740c4c68913e9dc-FC4495A9-0326-4FD6-EF2AA063DCBC0CDB', 'FC458205%2DFEDD%2D30D7%2D90A276511A417B5F', 'FC458204%2DAD51%2DDDF8%2D8E9C493201B6C437');
